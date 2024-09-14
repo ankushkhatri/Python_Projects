c843e31d6081906fab0ebef6e9a0cbc59e395cf1
Ankush <ankushkhatri.k@gmail.com>
2024-09-14 16:14:18 +0530

    commit

--- Automated Emails/main.py
+++ Automated Emails/main.py
@@ -1,12 +1,22 @@
 import yagmail
 import pandas
+import datetime
+import time
 from news import NewsFeed
 
+# while True:
+#     if datetime.datetime.now().hour == 15 and datetime.datetime.now().minute == 45:
 df = pandas.read_excel('people.xlsx')
 
 for index, row in df.iterrows():
-    news_feed = NewsFeed(interest=row['interest'], from_date='2024-08-20', to_date='2024-08-20', language='en')
+    news_feed = NewsFeed(interest = row['interest'], 
+                        from_date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d'), 
+                        to_date = datetime.datetime.now().strftime('%Y-%m-%d'), 
+                        language = 'en')
+    
     email = yagmail.SMTP(user="ankushkhatri.k@gmail.com", password="xqme tjiv oolc xxdd")
     email.send(to=row['email'], 
             subject=f"Your {row['interest']} news for today!",
-            contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. \n {news_feed.get()} \n Ash Mafia")
+            contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. \n\n {news_feed.get()} \n Ash Mafia")
+        
+    # time.sleep(60)
